5c02a7e921b82eaf38e957b6d8a772bd8bf13ee3
Satria-WS <extencezone02@gmail.com>
2023-05-08 07:05:13 +0700

    update json

--- server/index.js
+++ server/index.js
@@ -15,9 +15,15 @@ const port = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 
-mongoose.connect(process.env.MONGODB_URL).then(() => {
-  console.log("Mongdb connected");
-  server.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("Mongdb connected");
+    server.listen(port, () => {
+      console.log(`Server is listening on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log({ error });
+    process.exit(1);
   });
-}).catch;
